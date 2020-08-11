4a5ab80c5a19458890c5433dfaefe79596b6683e
Michael Sharpe <masrtis@gmail.com>
2020-08-10 23:29:39 -0400

    Convert frame buffer implementation to C++
    * Fix a buffer overflow when clearing VGA memory in framebuffer_clear()
    * Localize constants to function implementations where possible
    * Create a couple types to simplify working with the CPU frame buffer
    * auto and constexpr where appropriate
    * Replace static with anonymous namespace

--- kernel/io/framebuffer.cpp
+++ kernel/io/framebuffer.cpp
@@ -8,105 +8,160 @@
 #include <stddef.h>
 #include <stdint.h>
 
+namespace
+{
+
 /*
  *  Constants
  */
 
-enum VGA_GEOMETRY
-{
-    VGA_WIDTH = 80,
-    VGA_HEIGHT = 25
-};
+constexpr size_t VGA_WIDTH = 80;
+constexpr size_t VGA_HEIGHT = 25;
 
-enum BIT_SIZES
-{
-    NIBBLE = 4
-};
+uint16_t* const framebuffer = reinterpret_cast<uint16_t*>(0x000B8000);
 
-static const size_t BYTES_PER_ROW = VGA_WIDTH * sizeof(uint16_t);
+/*
+ *  Prototypes
+ */
 
-static uint16_t* const framebuffer = (uint16_t*)0x000B8000;
+constexpr size_t framebuffer_get_row_offset(size_t row);
+constexpr size_t framebuffer_get_index(size_t row, size_t column);
+constexpr uint16_t framebuffer_get_color(uint8_t fgColor, uint8_t bgColor);
+constexpr uint16_t framebuffer_compute_cell(unsigned char output, uint8_t fgColor, uint8_t bgColor);
 
 /*
  *  Types
  */
 
-enum framebuffer_bitmasks
+class framebuffer_position
 {
-    COLOR_MASK = 0x0F,
-    LOW_BYTE_MASK = 0x000000FF,
-    HIGH_BYTE_MASK = 0x0000FF00
+public:
+    void move_cursor() const
+    {
+        framebuffer_move_cursor(row, column, FRAMEBUFFER_LIGHT_GRAY);
+    }
+
+    constexpr void advance_row()
+    {
+        ++row;
+        column = 0;
+    }
+
+    constexpr void advance_column()
+    {
+        ++column;
+    }
+
+    constexpr void scroll()
+    {
+        --row;
+    }
+
+    constexpr auto get_index() const
+    {
+        return framebuffer_get_index(row, column);
+    }
+
+    constexpr bool is_row_end() const
+    {
+        return column == VGA_WIDTH;
+    }
+
+    constexpr bool is_ready_to_scroll() const
+    {
+        return row == VGA_HEIGHT;
+    }
+private:
+    size_t row{0};
+    size_t column{0};
 };
 
-typedef struct framebuffer_position
+class framebuffer_data
 {
-    size_t row;
-    size_t column;
-} framebuffer_position;
+public:
+    constexpr void advance_row()
+    {
+        position.advance_row();
 
-typedef struct framebuffer_data
-{
-    uint16_t cells[VGA_WIDTH * VGA_HEIGHT];
+        if (position.is_ready_to_scroll())
+        {
+            constexpr size_t BYTES_PER_ROW = VGA_WIDTH * sizeof(uint16_t);
+
+            memmove(cells, cells + framebuffer_get_row_offset(1), FRAMEBUFFER_SIZE_IN_BYTES - BYTES_PER_ROW);
+            memset(cells + framebuffer_get_row_offset(VGA_HEIGHT - 1), 0, BYTES_PER_ROW);
+            position.scroll();
+        }
+    }
+
+    constexpr bool is_row_end() const
+    {
+        return position.is_row_end();
+    }
+
+    constexpr void write_cell(char output, uint8_t fgColor, uint8_t bgColor)
+    {
+        const size_t cellIndex = position.get_index();
+        cells[cellIndex] = framebuffer_compute_cell(output, fgColor, bgColor);
+        position.advance_column();
+    }
+
+    void clear()
+    {
+        memset(cells, 0, FRAMEBUFFER_SIZE_IN_BYTES);
+        position = framebuffer_position{};
+    }
+
+    void render() const
+    {
+        write_to_vga_memory();
+        position.move_cursor();
+    }
+private:
+    void write_to_vga_memory() const
+    {
+        memcpy(framebuffer, cells, FRAMEBUFFER_SIZE_IN_BYTES);
+    }
+
+    uint16_t cells[VGA_WIDTH * VGA_HEIGHT]{ 0 };
     framebuffer_position position;
-} framebuffer_data;
+public:
+    static constexpr size_t FRAMEBUFFER_SIZE_IN_BYTES = sizeof(cells);
+};
 
 /*
  *  Globals
  */
 
-static framebuffer_data framebuffer_cpu = { {0}, {0, 0} };
+framebuffer_data framebuffer_cpu;
 
 /*
  *  Local functions
  */
 
-static inline size_t framebuffer_get_row_offset(size_t row)
+constexpr size_t framebuffer_get_row_offset(size_t row)
 {
     return row * VGA_WIDTH;
 }
 
-static inline size_t framebuffer_get_index(size_t row, size_t column)
+constexpr size_t framebuffer_get_index(size_t row, size_t column)
 {
     return framebuffer_get_row_offset(row) + column;
 }
 
-static inline uint16_t framebuffer_get_color(uint8_t fgColor, uint8_t bgColor)
+constexpr uint16_t framebuffer_get_color(uint8_t fgColor, uint8_t bgColor)
 {
-    return (uint16_t)((fgColor & COLOR_MASK) | ((bgColor & COLOR_MASK) << NIBBLE));
+    constexpr size_t NIBBLE = 4;
+    constexpr uint8_t COLOR_MASK = 0x0F;
+    
+    return static_cast<uint16_t>((fgColor & COLOR_MASK) | ((bgColor & COLOR_MASK) << NIBBLE));
 }
 
-static inline uint16_t framebuffer_compute_cell(unsigned char output, uint8_t fgColor, uint8_t bgColor)
+constexpr uint16_t framebuffer_compute_cell(unsigned char output, uint8_t fgColor, uint8_t bgColor)
 {
-    return (uint16_t)output | (framebuffer_get_color(fgColor, bgColor) << CHAR_BIT);
+    return static_cast<uint16_t>(output) | (framebuffer_get_color(fgColor, bgColor) << CHAR_BIT);
 }
 
-static inline void framebuffer_position_advance_row(framebuffer_position* position)
-{
-    ++position->row;
-    position->column = 0;
-}
-
-static inline size_t framebuffer_position_get_index(const framebuffer_position* position)
-{
-    return framebuffer_get_index(position->row, position->column);
-}
-
-static inline void framebuffer_cpu_advance_row(framebuffer_data* cpuData)
-{
-    framebuffer_position_advance_row(&cpuData->position);
-
-    if (cpuData->position.row == VGA_HEIGHT)
-    {
-        memmove(cpuData->cells, cpuData->cells + framebuffer_get_row_offset(1), sizeof(cpuData->cells) - BYTES_PER_ROW);
-        memset(cpuData->cells + framebuffer_get_row_offset(VGA_HEIGHT - 1), 0, BYTES_PER_ROW);
-        --cpuData->position.row;
-    }
-}
-
-static inline void framebuffer_cpu_write_to_vga_memory(const framebuffer_data* cpuData)
-{
-    memcpy(framebuffer, cpuData->cells, sizeof(cpuData->cells));
-}
+} // end namespace
 
 /*
  *  Interface functions
@@ -119,21 +174,18 @@ void framebuffer_write_cell(size_t row, size_t column, unsigned char output, uin
 
 void framebuffer_move_cursor(size_t row, size_t column, uint8_t fgColor)
 {
-    enum framebuffer_io_ports
-    {
-        FRAMEBUFFER_COMMAND_PORT = 0x3D4,
-        FRAMEBUFFER_DATA_PORT = 0x3D5
-    };
+    constexpr uint16_t FRAMEBUFFER_COMMAND_PORT = 0x3D4;
+    constexpr uint16_t FRAMEBUFFER_DATA_PORT = 0x3D5;
 
-    enum framebuffer_io_commands
-    {
-        FRAMEBUFFER_HIGH_BYTE_COMMAND = 14,
-        FRAMEBUFFER_LOW_BYTE_COMMAND = 15
-    };
+    constexpr uint8_t FRAMEBUFFER_HIGH_BYTE_COMMAND = 14;
+    constexpr uint8_t FRAMEBUFFER_LOW_BYTE_COMMAND = 15;
 
     framebuffer_write_cell(row, column, 0, fgColor, FRAMEBUFFER_BLACK);
 
-    const size_t position = framebuffer_get_index(row, column);
+    const auto position = framebuffer_get_index(row, column);
+
+    constexpr uint16_t LOW_BYTE_MASK = 0xFF;
+    constexpr uint16_t HIGH_BYTE_MASK = 0xFF00;
 
     outb(FRAMEBUFFER_COMMAND_PORT, FRAMEBUFFER_HIGH_BYTE_COMMAND);
     outb(FRAMEBUFFER_DATA_PORT, (position & HIGH_BYTE_MASK) >> CHAR_BIT);
@@ -143,8 +195,8 @@ void framebuffer_move_cursor(size_t row, size_t column, uint8_t fgColor)
 
 void framebuffer_clear()
 {
-    memset(framebuffer, 0, sizeof(framebuffer_cpu));
-    memset(&framebuffer_cpu, 0, sizeof(framebuffer_cpu));
+    memset(framebuffer, 0, framebuffer_data::FRAMEBUFFER_SIZE_IN_BYTES);
+    framebuffer_cpu.clear();
 }
 
 void framebuffer_write(const char* string)
@@ -154,13 +206,13 @@ void framebuffer_write(const char* string)
 
 void framebuffer_write_color(const char* string, uint8_t fgColor, uint8_t bgColor)
 {
-    const size_t len = strlen(string);
+    const auto len = strlen(string);
 
     for (size_t i = 0; i <= len; ++i)
     {
-        if (string[i] == '\n' || framebuffer_cpu.position.column == VGA_WIDTH)
+        if (string[i] == '\n' || framebuffer_cpu.is_row_end())
         {
-            framebuffer_cpu_advance_row(&framebuffer_cpu);
+            framebuffer_cpu.advance_row();
 
             if (string[i] == '\n')
             {
@@ -168,18 +220,13 @@ void framebuffer_write_color(const char* string, uint8_t fgColor, uint8_t bgColo
             }
         }
 
-        const size_t cellIndex = framebuffer_position_get_index(&framebuffer_cpu.position);
-        framebuffer_cpu.cells[cellIndex] = framebuffer_compute_cell(string[i], fgColor, bgColor);
-
-        ++framebuffer_cpu.position.column;
+        framebuffer_cpu.write_cell(string[i], fgColor, bgColor);
     }
 
-    if (framebuffer_cpu.position.column == VGA_WIDTH)
+    if (framebuffer_cpu.is_row_end())
     {
-        framebuffer_cpu_advance_row(&framebuffer_cpu);
+        framebuffer_cpu.advance_row();
     }
 
-    framebuffer_cpu_write_to_vga_memory(&framebuffer_cpu);
-
-    framebuffer_move_cursor(framebuffer_cpu.position.row, framebuffer_cpu.position.column, FRAMEBUFFER_LIGHT_GRAY);
+    framebuffer_cpu.render();
 }
--- kernel/kmain.cpp
+++ kernel/kmain.cpp
@@ -4,7 +4,7 @@ extern "C" int kmain()
 {
     framebuffer_clear();
 
-    framebuffer_write_color("0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25\n", FRAMEBUFFER_WHITE, FRAMEBUFFER_GREEN);
+    framebuffer_write_color("0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25", FRAMEBUFFER_WHITE, FRAMEBUFFER_GREEN);
 
     return 0xDEADDEAD;
 }
