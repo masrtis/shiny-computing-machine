b1b2ba38032fd0a9365ffb285de61618056095e8
Michael Sharpe <masrtis@gmail.com>
2020-08-17 00:10:02 -0400

    Improve readability of loader source
    * Add some flags to prepare for memory mapping and paging
    * Separate the multiboot header from the text section in the assembly
    ** Adjust link.ld to make sure multiboot section is still placed within the .text file in the ELF
    * Expand kernel stack size to 16 KB

--- loader/loader.asm
+++ loader/loader.asm
@@ -4,21 +4,28 @@ extern _init
 extern _fini
 
 MAGIC_NUMBER equ 0x1BADB002
-FLAGS equ 0x0
-CHECKSUM equ -MAGIC_NUMBER
+ALIGN_MODULE_LOAD_TO_PAGE_BOUNDARY equ (1 << 0)
+PROVIDE_MEMORY_MAP equ (1 << 1)
+FLAGS equ (ALIGN_MODULE_LOAD_TO_PAGE_BOUNDARY | PROVIDE_MEMORY_MAP)
+CHECKSUM equ -(MAGIC_NUMBER + FLAGS)
 
-KERNEL_STACK_SIZE equ 4096
+KB_SIZE equ 1024
+
+KERNEL_STACK_SIZE equ 16 * KB_SIZE
+
+section .multiboot
+align 4
+    dd MAGIC_NUMBER
+    dd FLAGS
+    dd CHECKSUM
 
 section .bss
-align 4
+align 16
 kernel_stack: ; beginning of stack memory
     resb KERNEL_STACK_SIZE
 
 section .text
 align 4
-    dd MAGIC_NUMBER
-    dd FLAGS
-    dd CHECKSUM
 
 loader:
     mov esp, kernel_stack + KERNEL_STACK_SIZE
